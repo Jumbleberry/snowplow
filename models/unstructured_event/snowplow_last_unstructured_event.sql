b9eaeede3db101ab596de27e663e0e86c2f46e17
park0450 <park0450@mylaurier.ca>
2019-10-29 15:46:43 -0400

    Update snowplow_last_unstructured_event.sql

--- models/unstructured_event/snowplow_last_unstructured_event.sql
+++ models/unstructured_event/snowplow_last_unstructured_event.sql
@@ -7,56 +7,36 @@
     )
 }}
 
-with events as (
-    select * from {{ var('snowplow:events') }}
-),
-
-link_click as (
-    select * from {{ ref('snowplow_link_click') }}
-),
-
-submit_form as (
-    select * from {{ ref('snowplow_submit_form') }}
-),
-
-change_form as (
-    select * from {{ ref('snowplow_change_form') }}
-),
-
-
-prep as (
+with prep as (
     SELECT
     ev.event,
     ev.event_id,
     ev.domain_userid,
     ev.collector_tstamp,
-    lc.target_url as link_clicked_url,
-    sf.form_id as form_name,
-    cf.element_id as form_field_name,
     CASE WHEN ROW_NUMBER() OVER (PARTITION BY ev.domain_userid ORDER BY ev.collector_tstamp DESC) = '1' AND EVENT = 'ue' THEN '1' ELSE '0' END as de_dupe
-    FROM events as ev
-    
-    LEFT JOIN link_click as lc
-    ON ev.event_id = lc.event_id
-    
-    LEFT JOIN submit_form as sf
-    ON ev.event_id = sf.event_id
-    
-    LEFT JOIN change_form as cf
-    ON ev.event_id = sf.event_id
-    
-    GROUP BY 1,2,3,4,5,6,7
+    FROM {{ ref('snowplow_base_events') }} as ev
+    GROUP BY 1,2,3,4
 ),
 
-de_dupe as (
-    SELECT 
-    domain_userid,
-    link_clicked_url,
-    form_name,
-    form_field_name
-    FROM prep
-    WHERE de_dupe = '1'
+joined as (
+
+    select 
+    p.domain_userid,
+    lc.target_url as link_clicked_url,
+    sf.form_id as form_name,
+    cf.element_id as form_field_name
+    from prep p
+    
+    left join {{ var('snowplow:link_click') }} as lc
+    on p.event_id = lc.event_id
+    left join {{ var('snowplow:submit_form') }} as sf
+    on p.event_id = sf.event_id
+    left join {{ var('snowplow:change_form') }} as cf
+    on p.event_id = cf.event_id
+    
+    WHERE p.de_dupe = '1'
 )
 
-select * from de_dupe
+select * from joined
+
 
