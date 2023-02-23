a3f94162d68dbe50583180140a15eed103842379
Cidro <pablo.canaleshernandez@gmail.com>
2023-02-23 17:58:54 -0300

    add timeframe for deb runs

--- models/fivetran/sp_base_events_fivetran.sql
+++ models/fivetran/sp_base_events_fivetran.sql
@@ -20,7 +20,7 @@ with events as (
     select * from {{ var('snowplow:events') }}
 
     {% if target.name == 'dev' %}
-      where collector_tstamp >= dateadd('day', -3, current_date)
+      where collector_tstamp between dbt_utils.dateadd('month', -2, current_date) and dbt_utils.dateadd('month', -1, current_date)
     {% endif %}
 
 ),
